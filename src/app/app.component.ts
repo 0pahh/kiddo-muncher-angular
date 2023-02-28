5b3064511ee439d9adc296270da88fcd59179d0c
0pahh <Thierry.Angel@protonmail.com>
2023-02-28 15:02:52 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kiddo-muncher-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kiddo-muncher';
+}
