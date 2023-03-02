75967332a65b5243b1aa448fbedac8c0b34b454b
0pahh <Thierry.Angel@protonmail.com>
2023-03-02 10:54:26 +0100

    working but not optimized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,31 @@ import { Simulation } from './models/Simulation';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'kiddo-muncher';
   test = new Simulation();
   simulation2: any[][] = [];
-  async ngOnInit(): Promise<void> {
-    const simulation = this.test.returnSimulation();
-    // this.simulation2 = simulation[0];
+  start = false;
 
-    for (const array of simulation) {
-      this.simulation2 = array;
-      await new Promise((resolve) => setTimeout(resolve, 250)); // Wait for 500ms before logging the next array
+  interval: any;
+
+  startSimulation(): void {
+    this.start = !this.start;
+    if (this.start) {
+      let simulation = this.test.returnSimulation();
+      let i = 0;
+      this.interval = setInterval(() => {
+        if (i === simulation.length - 1) {
+          clearInterval(this.interval);
+          this.start = false;
+        }
+        this.simulation2 = simulation[i];
+        i++;
+      }, 110);
+    } else {
+      this.simulation2 = [];
+
+      if (this.interval) clearInterval(this.interval);
     }
   }
 }
